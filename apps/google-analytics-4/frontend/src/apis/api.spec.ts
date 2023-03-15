4c1122ba1cd2b43b668863d886e60ab7e3e30cb7
gordo-contentful <matt.gordon@contentful.com>
2023-03-15 16:12:24 -0600

    [WIP] docker compose setup working, added hardcoded data for tests error, update sls config

--- apps/google-analytics-4/frontend/src/apis/api.spec.ts
+++ apps/google-analytics-4/frontend/src/apis/api.spec.ts
@@ -6,7 +6,163 @@ import { mockCma, validServiceKeyFile, validServiceKeyId } from '../../test/mock
 import { mockAccountSummary } from '../../test/mocks/api/mockData';
 import { ContentfulContext } from 'types';
 import { fetchFromApi } from 'apis/fetchApi';
-import { runReportData } from '../../../lambda/public/sampleData/MockData';
+// import { runReportData } from '../../../lambda/public/sampleData/MockData';
+
+export const runReportData = {
+  dimensionHeaders: [
+    {
+      name: 'date',
+    },
+  ],
+  metricHeaders: [
+    {
+      name: 'screenPageViews',
+      type: 'TYPE_INTEGER',
+    },
+  ],
+  rows: [
+    {
+      dimensionValues: [
+        {
+          value: '20230215',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '25',
+          oneValue: 'value',
+        },
+      ],
+    },
+    {
+      dimensionValues: [
+        {
+          value: '20230216',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '9',
+          oneValue: 'value',
+        },
+      ],
+    },
+    {
+      dimensionValues: [
+        {
+          value: '20230217',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '51',
+          oneValue: 'value',
+        },
+      ],
+    },
+    {
+      dimensionValues: [
+        {
+          value: '20230218',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '25',
+          oneValue: 'value',
+        },
+      ],
+    },
+    {
+      dimensionValues: [
+        {
+          value: '20230219',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '5',
+          oneValue: 'value',
+        },
+      ],
+    },
+    {
+      dimensionValues: [
+        {
+          value: '20230220',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '42',
+          oneValue: 'value',
+        },
+      ],
+    },
+    {
+      dimensionValues: [
+        {
+          value: '20230221',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '6',
+          oneValue: 'value',
+        },
+      ],
+    },
+    {
+      dimensionValues: [
+        {
+          value: '20230222',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '18',
+          oneValue: 'value',
+        },
+      ],
+    },
+    {
+      dimensionValues: [
+        {
+          value: '20230223',
+          oneValue: 'value',
+        },
+      ],
+      metricValues: [
+        {
+          value: '14',
+          oneValue: 'value',
+        },
+      ],
+    },
+  ],
+  totals: [],
+  maximums: [],
+  minimums: [],
+  rowCount: 9,
+  metadata: {
+    dataLossFromOtherRow: false,
+    currencyCode: 'USD',
+    _currencyCode: 'currencyCode',
+    timeZone: 'Europe/Berlin',
+    _timeZone: 'timeZone',
+  },
+  propertyQuota: null,
+  kind: 'analyticsData#runReport',
+};
+
 
 describe('fetchFromApi()', () => {
   const ZSomeSchema = z.object({ foo: z.string() });
--- apps/google-analytics-4/frontend/src/components/main-app/analytics-app/AnalyticsApp.spec.tsx
+++ apps/google-analytics-4/frontend/src/components/main-app/analytics-app/AnalyticsApp.spec.tsx
@@ -1,7 +1,193 @@
 import AnalyticsApp from './AnalyticsApp';
 import { render, screen } from '@testing-library/react';
-import runReportResponseHasViews from '../../../../../lambda/public/sampleData/runReportResponseHasViews.json';
-import runReportResponseNoView from '../../../../../lambda/public/sampleData/runReportResponseNoViews.json';
+// import runReportResponseHasViews from '../../../../../lambda/public/sampleData/runReportResponseHasViews.json';
+// import runReportResponseNoView from '../../../../../lambda/public/sampleData/runReportResponseNoViews.json';
+
+
+
+const runReportResponseHasViews = {
+  "dimensionHeaders": [
+    {
+      "name": "date"
+    }
+  ],
+  "metricHeaders": [
+    {
+      "name": "screenPageViews",
+      "type": "TYPE_INTEGER"
+    }
+  ],
+  "rows": [
+    {
+      "dimensionValues": [
+        {
+          "value": "20230215",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "25",
+          "oneValue": "value"
+        }
+      ]
+    },
+    {
+      "dimensionValues": [
+        {
+          "value": "20230216",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "9",
+          "oneValue": "value"
+        }
+      ]
+    },
+    {
+      "dimensionValues": [
+        {
+          "value": "20230217",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "51",
+          "oneValue": "value"
+        }
+      ]
+    },
+    {
+      "dimensionValues": [
+        {
+          "value": "20230218",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "25",
+          "oneValue": "value"
+        }
+      ]
+    },
+    {
+      "dimensionValues": [
+        {
+          "value": "20230219",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "5",
+          "oneValue": "value"
+        }
+      ]
+    },
+    {
+      "dimensionValues": [
+        {
+          "value": "20230220",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "42",
+          "oneValue": "value"
+        }
+      ]
+    },
+    {
+      "dimensionValues": [
+        {
+          "value": "20230221",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "6",
+          "oneValue": "value"
+        }
+      ]
+    },
+    {
+      "dimensionValues": [
+        {
+          "value": "20230222",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "18",
+          "oneValue": "value"
+        }
+      ]
+    },
+    {
+      "dimensionValues": [
+        {
+          "value": "20230223",
+          "oneValue": "value"
+        }
+      ],
+      "metricValues": [
+        {
+          "value": "14",
+          "oneValue": "value"
+        }
+      ]
+    }
+  ],
+  "totals": [],
+  "maximums": [],
+  "minimums": [],
+  "rowCount": 9,
+  "metadata": {
+    "dataLossFromOtherRow": false,
+    "currencyCode": "USD",
+    "_currencyCode": "currencyCode",
+    "timeZone": "Europe/Berlin",
+    "_timeZone": "timeZone"
+  },
+  "propertyQuota": null,
+  "kind": "analyticsData#runReport"
+}
+
+const runReportResponseNoView = {
+  "dimensionHeaders": [
+    {
+      "name": "date"
+    }
+  ],
+  "metricHeaders": [
+    {
+      "name": "screenPageViews",
+      "type": "TYPE_INTEGER"
+    }
+  ],
+  "rows": [],
+  "totals": [],
+  "maximums": [],
+  "minimums": [],
+  "rowCount": 0,
+  "metadata": {
+    "dataLossFromOtherRow": false,
+    "currencyCode": "USD",
+    "_currencyCode": "currencyCode",
+    "timeZone": "Europe/Berlin",
+    "_timeZone": "timeZone"
+  },
+  "propertyQuota": null,
+  "kind": "analyticsData#runReport"
+}
+
 
 jest.mock('@contentful/react-apps-toolkit', () => ({
   useAutoResizer: () => jest.fn(),
