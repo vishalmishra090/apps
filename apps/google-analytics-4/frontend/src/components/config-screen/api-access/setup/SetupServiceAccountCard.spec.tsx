6099c406f98e2c6cc9082a2db36f6445a968f52b
Matt Gordon <matt.gordon@contentful.com>
2023-04-06 11:25:04 -0600

    [WIP] :bug: missed var

--- apps/google-analytics-4/frontend/src/components/config-screen/api-access/setup/SetupServiceAccountCard.spec.tsx
+++ apps/google-analytics-4/frontend/src/components/config-screen/api-access/setup/SetupServiceAccountCard.spec.tsx
@@ -1,4 +1,4 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SetupServiceAccountCard from 'components/config-screen/api-access/setup/SetupServiceAccountCard';
 import { mockSdk, mockCma, validServiceKeyFile } from '../../../../../test/mocks';
